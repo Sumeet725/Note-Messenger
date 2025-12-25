ddcad56fe5c58dcf309634ffb3f2cf0702d53e2d
Sumeet725 <167642750+Sumeet725@users.noreply.github.com>
2025-12-25 22:29:13 +0530

    port updation

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def retrieve_note():
     return jsonify({"text": text}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
